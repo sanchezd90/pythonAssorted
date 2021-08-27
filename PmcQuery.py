52a8864eef041997e44b8c7bc7a4db090f66d4f5
sanchezd90 <sanchezd90@gmail.com>
2021-08-27 11:53:09 -0300

    new methods for Search and Fetch
    Search can get custom fields form every id found using Fetch

--- PmcQuery.py
+++ PmcQuery.py
@@ -20,9 +20,22 @@ class Search():
         return requests.get(self.url())
     def stripIdElement(self,idElement):
         return idElement.strip("<Id>").strip("</Id>")    
-    def idList(self):
+    def getIds(self):
         idString=re.split("IdList",self.response().content.decode('UTF-8'))[1][1:-2]
-        return [self.stripIdElement(x) for x in idString.split("\n")][1:-1]    
+        return [self.stripIdElement(x) for x in idString.split("\n")][1:-1]
+    def getData(self,*args):
+        results=[]
+        for id in self.getIds():
+            f=Fetch(id)
+            results.append(f.get(*args))
+        return results
+    def saveData(self,*args):
+        results=[]
+        for id in self.getIds():
+            f=Fetch(id)
+            results.append(f.get(*args))        
+        with open(f"{self.term}.json","w",encoding="utf-8") as f:
+            f.write(json.dumps(results,indent=4))
 
 class Fetch():
 
@@ -45,4 +58,101 @@ class Fetch():
             f.write(self.toJson())
     def toDict(self):
         return json.loads(self.toJson())
+    def get(self,*args):
+        data=self.toDict()["pmc-articleset"]["article"]        
+        fieldDict= {}
+        for arg in args:
+            #--- ALL ---#
+            if arg == "all":
+                try:
+                    fieldDict["journal-title"]=data["front"]["journal-meta"]["journal-title-group"]["journal-title"]
+                except Exception as e:
+                    fieldDict["journal-title"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+            
+                try:
+                    for ids in data["front"]["article-meta"]["article-id"]:
+                            if ids["@pub-id-type"]=="doi":
+                                fieldDict["doi"]=ids["#text"]
+                except Exception as e:
+                    fieldDict["doi"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+            
+                try:
+                    fieldDict["title"]=data["front"]["article-meta"]["title-group"]["article-title"]
+                except Exception as e:
+                    fieldDict["title"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+           
+                try:                    
+                    fieldDict["date"]=data["front"]["article-meta"]["pub-date"][0]
+                except Exception as e:
+                    fieldDict["date"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+            
+                try:
+                    fieldDict["abstract"]=data["front"]["article-meta"]["abstract"]["p"]["#text"]
+                except Exception as e:
+                    fieldDict["abstract"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+           
+                try:
+                    fieldDict["keywords"]=data["front"]["article-meta"]["kwd-group"]["kwd"]
+                except Exception as e:
+                    fieldDict["keywords"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+
+                return fieldDict 
+            #--- JOURNAL TITLE ---#
+            elif arg == "journal-title":
+                try:
+                    fieldDict["journal-title"]=data["front"]["journal-meta"]["journal-title-group"]["journal-title"]
+                except Exception as e:
+                    fieldDict["journal-title"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+            
+            #--- DOI ---#
+            elif arg == "doi":
+                try:
+                    for ids in data["front"]["article-meta"]["article-id"]:
+                            if ids["@pub-id-type"]=="doi":
+                                fieldDict["doi"]=ids["#text"]
+                except Exception as e:
+                    fieldDict["doi"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+            
+            #--- TITLE ---#
+            elif arg == "title":
+                try:
+                    fieldDict["title"]=data["front"]["article-meta"]["title-group"]["article-title"]
+                except Exception as e:
+                    fieldDict["title"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+           
+            #--- DATE ---#
+            elif arg == "date":
+                try:                    
+                    fieldDict["date"]=data["front"]["article-meta"]["pub-date"][0]
+                except Exception as e:
+                    fieldDict["date"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+            
+            #--- ABSTRACT ---#
+            elif arg == "abstract":
+                try:
+                    fieldDict["abstract"]=data["front"]["article-meta"]["abstract"]["p"]["#text"]
+                except Exception as e:
+                    fieldDict["abstract"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+            
+            #--- KEYWORDS ---#
+            elif arg == "keywords":
+                try:
+                    fieldDict["keywords"]=data["front"]["article-meta"]["kwd-group"]["kwd"]
+                except Exception as e:
+                    fieldDict["keywords"]="notFound"
+                    print(f"Error in '{arg}' search ",e)
+        return fieldDict
+
+
 
